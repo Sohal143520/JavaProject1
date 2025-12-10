70ac95e5e08639c125bafc444166b72c237bfea3
Sohal143520 <Harmindersinghsohal45@gmail.com>
2025-12-10 19:48:25 +0530

    updating adding input

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class Main{
 	public static void main(String[] args){
-		String name = "harminder";
-		System.out.println("hallo "+name);
+		String name = null;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name here:");
+		name = sc.nextLine();
+		System.out.printf("Hello %s",name);
 	}
 }
